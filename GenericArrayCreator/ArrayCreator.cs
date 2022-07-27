0a7662755367b07ed475e2eeaae8d307d19953f6
Dimo Todorov <baal98@gmail.com>
2022-07-28 00:28:05 +0300

    Add files via upload

---
+++ GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int count, T element)
+        {
+            T[] result = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = element;
+            }
+
+            return result;
+        }
+    }
+}
