a2b5b8fec4aed8cfc30a7efcf7191a5151a504a1
Dimo Todorov <baal98@gmail.com>
2022-07-27 07:11:04 +0300

    Add files via upload

---
+++ Box of T/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public void Add(T element)
+        {
+            internalList.Add(element);
+        }
+
+        public int Count => internalList.Count;
+
+        public T Remove()
+        {
+            T element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+            return element;
+        }
+    }
+}
---
+++ Box of T/EqualityScale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class EqualityScale<T>
+    // where T : class
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            // Equals -> left == right (class)
+            // Equals -> byte-to-byte (struct)
+            return left.Equals(right);
+        }
+    }
+}
