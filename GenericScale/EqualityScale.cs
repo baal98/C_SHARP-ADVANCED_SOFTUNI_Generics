22791d9356a35c9a84229fa4158b1f3669a2bc12
Dimo Todorov <baal98@gmail.com>
2022-07-29 21:03:33 +0300

    Add files via upload

---
+++ GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            // Equals -> left == right (class)
+            // Equals -> byte-to-byte (struct)
+            return left.Equals(right);
+        }
+    }
+}
