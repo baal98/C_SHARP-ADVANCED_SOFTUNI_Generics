29d0cef25fc61538d3dc6059c72faad7174685fa
Dimo Todorov <baal98@gmail.com>
2022-07-29 21:02:45 +0300

    Add files via upload

---
+++ GenericCountMethodStrings/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+        public int Count(T element)
+        {
+            int count = 0;
+            foreach (T item in list)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
---
+++ GenericCountMethodStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+            
+            string element = Console.ReadLine();
+
+            Console.WriteLine(box.Count(element));
+
+        }
+    }
+}
