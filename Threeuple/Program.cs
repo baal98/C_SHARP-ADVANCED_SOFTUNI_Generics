6fa2903a97b97fb1b74e34fd543fd8d981617f21
Dimo Todorov <baal98@gmail.com>
2022-07-30 22:03:53 +0300

    Add files via upload

---
+++ Threeuple/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Threeuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Threeuple<string, string, string> nameAddress = new Threeuple<string, string, string>("", "", "");
+            Threeuple<string, int, bool> nameBeer = new Threeuple<string, int, bool>("", 0, true);
+            Threeuple<string, double, string> nameBank = new Threeuple<string, double, string>("", 0, "");
+
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+
+            nameAddress.ItemLeft = input1.Split(" ")[0] + " " + input1.Split(" ")[1];
+            nameAddress.ItemMiddle = input1.Split(" ")[2];
+            nameAddress.ItemRight = "";
+            int ddd = input1.Split(" ").Length;
+            for (int i = 3; i < input1.Split(" ").Length; i++)
+            {
+                nameAddress.ItemRight += input1.Split(" ")[i] + " ";
+            }
+            //nameAddress.ItemRight = string.Join(" ", input1.Skip(3));
+            Console.WriteLine($"{nameAddress.ItemLeft} -> {nameAddress.ItemMiddle} -> {nameAddress.ItemRight}");
+
+            nameBeer.ItemLeft = input2.Split(" ")[0];
+            nameBeer.ItemMiddle = int.Parse(input2.Split(" ")[1]);
+            string isDrunk = "";
+            if (input2.Split(" ")[2] == "drunk")
+            {
+                nameBeer.ItemRight = true;
+                isDrunk = "True";
+            }
+            else
+            {
+                nameBeer.ItemRight = false;
+                isDrunk = "False";
+            }
+            Console.WriteLine($"{nameBeer.ItemLeft} -> {nameBeer.ItemMiddle} -> {isDrunk}");
+
+            nameBank.ItemLeft = input3.Split(" ")[0];
+            nameBank.ItemMiddle = double.Parse(input3.Split(" ")[1]);
+            nameBank.ItemRight = input3.Split(" ")[2];
+            Console.WriteLine($"{nameBank.ItemLeft} -> {nameBank.ItemMiddle} -> {nameBank.ItemRight}");
+        }
+    }
+}
---
+++ Threeuple/Threeuple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class Threeuple<Item1, Item2, Item3>
+    {
+        public Threeuple(Item1 item1, Item2 item2, Item3 item3)
+        {
+            ItemLeft = item1;
+            ItemMiddle = item2;
+            ItemRight = item3;
+        }
+        public Item1 ItemLeft { get; set; }
+        public Item2 ItemMiddle { get; set; }
+        public Item3 ItemRight { get; set; }
+
+    }
+}
